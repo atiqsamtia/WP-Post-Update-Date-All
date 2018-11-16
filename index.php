ca257d4407f63e9bb4247c92e5f687f71ae7e855
Atiq Samtia <atiq@falconspk.net>
2018-11-16 12:56:12 +0500

    Time Interval Support Categories support.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Bulk Post Update Date
- * Version: 1.0.2
+ * Version: 1.1.0
  * Description: Change the Post Update date for all posts in one click. This will help your blog in search engines and your blog will look alive. Do this every week or month. (Tip By Waqas Jawed in Bloggers Funda - facebook group)
  * Author: Atiq Samtia
  * Author URI: http://atiqsamtia.com
@@ -40,24 +40,36 @@ function bulk_post_update_date_options() {
 	if ( isset( $_POST[ 'tb_refresh' ] ) && wp_verify_nonce( $_POST['tb_refresh'],'tb-refresh' ) && current_user_can( 'manage_options' ) ) {
 
         // get All posts IDs
-        //TODO: ADD categories and date filtering functionality in next Release.
+
+        $params = array(
+		        'numberposts' => -1,
+		        'fields'        => 'ids'
+	        );
+
+        if(isset( $_POST['categories'])){
+            $params['cat'] = implode( ',', $_POST['categories']);
+        }
 
         $ids = get_posts(
-            array(
-                'numberposts' => -1,
-                'fields'        => 'ids'
-            )
+                $params
         );
+
+        $from = $_POST['distribute'];
+        $to = time();
+
+        if($from == 0){
+            $range = explode( '-',$_POST['range']);
+            if(count( $range) == 2){
+                    $from = strtotime( $range[0]);
+                    $to = strtotime( $range[1]);
+            } else {
+                $from = strtotime( '-3 hours');
+            }
+        }
 
         foreach($ids as $id){
 
-            //Random Seconds to substract from time so that it would not set the same time for every post
-
-            $seconds = rand(1, 60 * 30);
-
-            $time = current_time('mysql');
-            $time = strtotime($time);
-            $time = $time - $seconds;
+	        $time = rand($from,$to);
             $time = date("Y-m-d H:i:s",$time);
 
             $wpdb->query(
@@ -67,12 +79,21 @@ function bulk_post_update_date_options() {
                      get_gmt_from_date($time),
                      $id
                     )
-                );   
+                );
         }
         $settings_saved = count($ids);
 	}
 
+	wp_enqueue_script('momentjs','https://cdn.jsdelivr.net/momentjs/latest/moment.min.js');
+	wp_enqueue_script('daterangepicker','https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js');
+	wp_enqueue_style('daterangepicker','https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css');
+
 	?>
+    <style>
+      .form-table input, .form-table select{
+          min-width: 250px;
+      }
+    </style>
 
 	<div class="wrap">
 		<h1><?php _e( 'Bulk Post Update Date', 'bulk-post-update-date' ); ?></h1>
@@ -81,20 +102,98 @@ function bulk_post_update_date_options() {
 				<p><strong><?php _e( "$settings_saved Posts Update date refreshed." ) ?></strong></p>
 			</div>
 		<?php endif ?>
-		<h2>
-			<?php _e( 'refresh the post modified date for all of your articles in one click, click REFRESH now', 'bulk-post-update-date' ) ?>
-		</h2>
+		<p>
+			<?php _e( 'Change the Post Update date for all posts in one click. This will help your blog in search engines and your blog will look alive. Do this every week or month.', 'bulk-post-update-date' ) ?>
+        </p>
+        <p>
+            <?php _e( 'A small yet powerful utility  provided by <a href="http://atiqsamtia.com">Atiq Samtia</a>', 'bulk-post-update-date' ) ?>
+		</p>
 		<form method="post" action="">
-			<div>
-				
 
-			</div>
+            <table class="form-table">
+
+                <tr valign="top">
+                    <th scope="row"><label for="distribute">Distribute into Last</label></th>
+                    <td>
+
+                        <select type="text" id="distribute" name="distribute">
+                            <option value="<?php echo strtotime( '-1 hour');?>">1 hour</option>
+                            <option value="<?php echo strtotime( '-1 day');?>">1 Day</option>
+                            <option value="<?php echo strtotime( '-15 days');?>">15 Days</option>
+                            <option value="<?php echo strtotime( '-1 month');?>">1 Month</option>
+                            <option value="<?php echo strtotime( '-2 month');?>">2 Months</option>
+                            <option value="<?php echo strtotime( '-3 month');?>">3 Months</option>
+                            <option value="<?php echo strtotime( '-6 month');?>">6 Months</option>
+                            <option value="0">Custom Range</option>
+                        </select>
+                        <p class="description">
+                            Select range of date in which you want to spread the dates of posts to look more realistic.
+                        </p>
+                    </td>
+                </tr>
+                <tr id="range_row" valign="top" style="display: none;">
+                    <th scope="row"><label for="range">Custom Date Range</label></th>
+                    <td>
+
+                        <input type="text" id="range" name="range" value="<?php echo date('m/d/y',strtotime( '-3 days'));?> - <?php echo date('m/d/y');?>" />
+                        <p class="description">
+                            Select range of date in which you want to spread the dates of posts to look more realistic.
+                        </p>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="categories">Select Categories</label></th>
+                    <td>
+
+                        <select multiple="multiple" id="categories" name="categories[]">
+
+		                    <?php
+		                    $args       = array(
+			                    'orderby' => 'name',
+                                'exclude' => '1'
+		                    );
+		                    $categories = get_categories( $args );
+		                    foreach ( $categories as $category ) { ?>
+
+                                <option value="<?php echo $category->term_id; ?>">
+				                    <?php echo $category->cat_name . ' (' . $category->category_count . ')'; ?>
+                                </option>
+		                    <?php } //endforeach ?>
+                        </select>
+                            <p class="description">
+                            Will apply on all posts if no category is selected. Select multiple categories by holding Ctrl or Command key while selecting.
+                        </p>
+                    </td>
+                </tr>
+
+            </table>
+
 			<p class="submit">
                 <input name="tb_refresh" type="hidden" value="<?php echo wp_create_nonce('tb-refresh'); ?>" />
-				<input class="button-primary" name="do" type="submit" value="<?php _e( 'Refresh' ) ?>" />
+				<input class="button-primary" name="do" type="submit" value="<?php _e( 'Update Post Dates' ) ?>" />
 			</p>
 		</form>
 	</div>
+
+    <script>
+        jQuery(function(){
+
+            jQuery('input[name="range"]').daterangepicker({
+                maxDate: '<?php echo date('m/d/y');?>'
+            });
+
+            jQuery('#distribute').change(function(){
+                let val = jQuery(this).val();
+                if(val == 0)
+                    jQuery('#range_row').fadeIn();
+                else
+                    jQuery('#range_row').fadeOut();
+
+            })
+
+        });
+    </script>
 
 <?php
 }
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: atiqsamtia
 Tags: posts, update, date, bulk, seo, google
 Donate link: https://www.fiverr.com/atiqsamtia/
 Requires at least: 3.0
-Tested up to: 4.9.5
+Tested up to: 4.9.8
 Requires PHP: 5.4
 Stable tag: trunk
 License: GPL2
@@ -16,7 +16,8 @@ Change the Post Update date for all posts in one click. This will help your blog
 
 If you update the posts manually, it would require a lot of effort weekly. If you have articles 500+ imagine how much time it would take you to do manually? This plugin will assist you in this regard and with only **one click** you are good to go.
 
-This Plugin also takes care of the same time and updates the time with the **random** value ranging from current time and 30 minutes in the past.
+This Plugin also takes care of the same time and updates the time with the **random** value ranging from current time and 3 hours in the past.
+It also has option to select custom time interval or custom date range as well. You can also update the date by selecting categories and it will only affect the posts in those categories.
 
 == Installation ==
 There are 3 methods to install this plugin
@@ -24,13 +25,13 @@ There are 3 methods to install this plugin
 2. Download this zip file and upload to your WordPress add new plugin page.
 3. Clone this repo and upload the files to your WordPress installation plugins folder via FTP.
 
-After you have installed the plugin, activate the plugin and go to Settings >> Bulk Post Update Date and click Refresh
+After you have installed the plugin, activate the plugin and go to Settings >> Bulk Post Update Date and click Update
 
 == Frequently Asked Questions ==
 =Is it safe to use this plugin?+
 Yes, It is perfectly fine to use this plugin, as it almost emulate the human activity with it's random timestamps
 
-=Can you modifyt he plugin accordong to my needs?=
+=Can you modify he plugin according to my needs?=
 Yes, I would love to. You can contact me via my website or my donation page.
 
 == Screenshots ==
@@ -39,6 +40,10 @@ Yes, I would love to. You can contact me via my website or my donation page.
 == Changelog ==
 =1.0.0=
 * Initial release.
+=1.1.0=
+* Added support for custom interval
+* Added support for custom date range
+* Added support for Categories
 
  == Upgrade Notice ==
 None
